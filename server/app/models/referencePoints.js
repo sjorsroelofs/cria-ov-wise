1eecc4a021ec925997927666c775de4741760789
Timk89 <t-kersten@hotmail.com>
2014-05-21 14:33:47 +0200

    Models voor routes, referencepoints en routedata

---
+++ server/app/models/referencePoints.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var referencePointSchema = new Schema({
+    id: { type int, required: true },
+    description: { type: String, required: true }
+})
+
+routeSchema.path('id').validate(function(val){
+    return(val.length > 4 && val !== undefined && val !== null);
+}, '');
+
+routeSchema.path('description').validate(function(val){
+    return(val.length > 200 && val !== undefined && val !== null);
+}, '');
+
+var modelName = "ReferencePoint";
+var collectionName = "referencePoints";
+mongoose.model(modelName, referencePointSchema, collectionName);
